d82dc29d881bf633cd02c9e6ccb1b6645ecc6e30
Brij Bhatia <brijbhatia997@gmail.com>
2024-06-23 23:53:36 -0700

    73. Set Matrix Zeroes

---
+++ python/73. Set Matrix Zeroes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m=len(matrix)
+        n=len(matrix[0])
+        
+        rows,cols=[],[]
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]==0:
+                    rows.append(i)
+                    cols.append(j)
+                    
+        for row in rows:
+            for j in range(n):
+                matrix[row][j]=0
+        
+        for i in range(m):
+            for col in cols:
+                matrix[i][col]=0
