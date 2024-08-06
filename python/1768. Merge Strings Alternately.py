542453fe977ad6c65e282f77e257eb998f570d0a
Brij Bhatia <brijbhatia997@gmail.com>
2024-08-06 16:44:39 -0700

    1768. Merge Strings Alternately

---
+++ python/1768. Merge Strings Alternately.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result=[]
+        for i,j in zip(word1,word2):
+            result.append(i+j)
+        if len(word1)>len(word2):
+            result.append(word1[len(word2):])
+        else:
+            result.append(word2[len(word1):])
+        return "".join(result)
