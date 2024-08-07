c29896ad9ea8544dd529c37dbb47cb20e19dd2a6
Brij Bhatia <brijbhatia997@gmail.com>
2024-08-06 19:57:17 -0700

    228. Summary Ranges

--- python/228. Summary Ranges.py
+++ python/228. Summary Ranges.py
@@ -3,11 +3,11 @@ class Solution:
         result=[]
         i=0
         while i<len(nums):
-            start=nums[i]
+            first=nums[i]
             while i<len(nums)-1 and nums[i]+1==nums[i+1]:
                 i+=1
-            if start!=nums[i]:
-                result.append(str(start)+"->"+str(nums[i]))
+            if first!=nums[i]:
+                result.append(str(first)+"->"+str(nums[i]))
             else:
                 result.append(str(nums[i]))
             i+=1
