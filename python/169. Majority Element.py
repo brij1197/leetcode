f2e9785b251321ee155d50755bea75a6ad0ff1ac
Brij Bhatia <brijbhatia997@gmail.com>
2024-08-08 02:47:25 -0700

    169. Majority Element

---
+++ python/169. Majority Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        majority=nums[0]
+        count=1
+        for i in range(1,len(nums)):
+            if majority!=nums[i] and count==0:
+                majority=nums[i]
+                count=1
+            elif majority==nums[i]:
+                count+=1
+            else:
+                count-=1
+        return majority
